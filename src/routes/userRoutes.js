33b673d271fe1598163015e32c2db1390f12d2cb
digambarkulu <108148485+digambarkulu@users.noreply.github.com>
2024-11-26 23:43:35 +0530

    install jwt token and generate access token and refresh token

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllUsers, createUser, deleteUser, updateUser } = require('../controllers/userController'); // Import controller
+const { getAllUsers, createUser, deleteUser, updateUser, loginUser } = require('../controllers/userController'); // Import controller
 const router = express.Router();
 
 // Define routes
@@ -7,5 +7,6 @@ router.get('/users', getAllUsers); // Route to get all users
 router.post('/users', createUser); // Route to create a new user
 router.delete('/users/:id', deleteUser); // Route to delete a user by ID
 router.put('/users/:id', updateUser); // Route to update a user by ID
+router.post('/login', loginUser); // Route for user login
 
 module.exports = router;
